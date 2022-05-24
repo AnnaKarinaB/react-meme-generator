c44e9ce21c23b5c73866c6746a8d7d89912e2509
Anna-Karina <annakarina.bernhart@gmail.com>
2022-05-24 09:26:07 +0200

    Final version

--- src/App.js
+++ src/App.js
@@ -1,78 +1,103 @@
+/** @jsxImportSource @emotion/react */
 import './App.css';
-import { useEffect, useState } from 'react';
-import Button from './Components/Button';
-import CustomMeme from './Components/CustomMeme';
-import Inputs from './Components/Inputs';
+import { css } from '@emotion/react';
+import { saveAs } from 'file-saver';
+import { useState } from 'react';
 
-function App() {
-  const [image, setImage] = useState('');
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
-  const [templates, setTemplates] = useState([]);
-  const [customMeme, setCustomMeme] = useState(
-    'https://api.memegen.link/images/doge/such_meme/very_skill.jpg?width=300',
-  );
+const buttonStyles = css`
+  margin-bottom: 1rem;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px auto;
+  font-size: 20px;
+  width: 150px;
+  height: 50px;
+`;
 
-  const url =
-    'https://api.memegen.link/images/doge/such_meme/very_skill.jpg?width=300';
+const imgStyles = css`
+  height: 350px;
+  width: 350px;
+`;
 
-  function handleChangeCustomMeme() {
-    setCustomMeme(
-      `https://api.memegen.link/images/${image}/${topText}/${bottomText}.png`,
-    );
-  }
+const buttonPosition = css`
+  display: flex;
+  border-radius: 8px;
+  font-size: 15px;
+  justify-content: center;
+  gap: 20px;
+  padding: 5px 10px 5px 10px;
+`;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url); // await fetch of url => returns a promise
-        const json = await response.json();
-        setTemplates(json);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+export function App() {
+  const [top, setTop] = useState('');
+  const [bottom, setBottom] = useState('');
+  const [template, setTemplate] = useState('');
 
-    // call the function
-    fetchData().catch((err) => {
-      console.log(err);
-    });
-  }, []);
+  const urlApi = `https://api.memegen.link/images/${
+    template ? template : 'doge'
+  }/${top ? top : '_'}/${bottom ? bottom : ''}.png`;
+  console.log(urlApi);
 
   return (
-    <main>
-      <section>
-        <h1>Meme generator</h1>
-        <Inputs
-          image={image}
-          setImage={setImage}
-          topText={topText}
-          setTopText={setTopText}
-          bottomText={bottomText}
-          setBottomText={setBottomText}
-        />
-        <Button
-          image={image}
-          setImage={setImage}
-          topText={topText}
-          setTopText={setTopText}
-          bottomText={bottomText}
-          setBottomText={setBottomText}
-          url={url}
-          handleChangeCustomMeme={handleChangeCustomMeme}
-          templates={templates}
-        />
-        <CustomMeme
-          customMeme={customMeme}
-          setCustomMeme={setCustomMeme}
-          image={image}
-          topText={topText}
-          bottomText={bottomText}
-          handleChangeCustomMeme={handleChangeCustomMeme}
-        />
-      </section>
-    </main>
+    <div className="App">
+      <h1>React Meme Generator</h1>
+
+      <div>
+        <label>
+          Choose template:
+          <br />
+          <input
+            value={template}
+            onChange={(event) => {
+              setTemplate(event.currentTarget.value);
+            }}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Top text:
+          <br />
+          <input
+            value={top}
+            onChange={(event) => {
+              setTop(event.currentTarget.value);
+            }}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Bottom text:
+          <br />
+          <input
+            value={bottom}
+            onChange={(event) => {
+              setBottom(event.currentTarget.value);
+            }}
+          />
+        </label>
+      </div>
+      <br />
+      <img
+        css={imgStyles}
+        data-test-id="meme-image"
+        src={urlApi}
+        alt="generated-meme"
+      />
+      <div css={buttonPosition}>
+        <button
+          css={buttonStyles}
+          onClick={() => {
+            saveAs(urlApi, 'meme.jpg');
+          }}
+        >
+          Download
+        </button>
+      </div>
+    </div>
   );
 }
-
 export default App;
